2e5bc638c50d37e4eb9b3f47aec1579843171bdd
Dan Kalinin <daniil551@hotmail.com>
2019-01-10 21:51:58 +0300

    main

---
+++ UIKitExt/UIEMain.h
@@ -0,0 +1,8 @@
+//
+//  UIEMain.h
+//  UIKitExt
+//
+//  Created by Dan Kalinin on 1/10/19.
+//
+
+#import <Foundation/Foundation.h>
---
+++ UIKitExt/UIEMain.m
@@ -0,0 +1,8 @@
+//
+//  UIEMain.m
+//  UIKitExt
+//
+//  Created by Dan Kalinin on 1/10/19.
+//
+
+#import "UIEMain.h"
